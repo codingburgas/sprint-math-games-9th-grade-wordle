93285858a253c2c2d6e599fa0a4b5ddfcf716709
NIIvanov24 <NIIvanov24@codingburgas.bg>
2025-11-27 22:50:15 +0200

    Fix some errors

--- Wordle++/main.cpp
+++ Wordle++/main.cpp
@@ -2,43 +2,49 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <fstream>
 using namespace std;
- const int Word_Length = 5;
- const int Not_Matched = 0;
- const int Parital_Matched = 1;
- const int Matched = 2;
- const int Number_of_guessed = 6;
- void toUpperCase(string&input){
-    transform(input.begin(), input.end(), input.begin(), [](unsigned char c ){return toupper(c); }); 
- }
- string getRandomWord(){
+const int Word_Length = 5;
+const int Not_Matched = 0;
+const int Parital_Matched = 1;
+const int Matched = 2;
+const int Number_of_guessed = 6;
+void toUpperCase(string& input) {
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c) { return toupper(c); });
+}
+string getRandomWord() {
     return"ready";
 
-    bool isValidWord(string word){
-        return word.legnth() == Word_Length && word.find_first_not_of(Ptr: "ABCDEFGHIJKLMNOPQRSTUVWXYZ") == string::npos;
+    bool isValidWord(const string & word) {
+        if ((int)word.length() != Word_Length) return false;
+        for (char c : word) {
+            if (c < 'A' || c > 'Z') return false;
+        }
+        return true;
     }
- }
-int main(){
-vector<string> guesses(Number_of_guessed);
-vector<vector<int>> matches(Number_of_guessed);
-string targetWord = getRandomWord();
-int currentguesscount = 0;
-string input;
-int currentTry = 0;
-while (currentTry < Number_of_guessed)
-{
-    do
-    {
-        cout << "Please enter your guess (word length must be " + to_string(Word_Length) + ") or type Q to quit: ";
-        getline(cin, input);
-        toUpperCase(input);
-    } while (input != "Q" && !isValid(input));
-    if (input == "Q")
-    {
-        cout << "Quit game" << endl;
-        break;
-    }
-    tries[currentTry] = input;
-    currentTry++;
+    string getRandomWord() {};
 }
+int main() {
+    vector<string> guesses(Number_of_guessed);
+    vector<vector<int>> matches(Number_of_guessed);
+    string targetWord = getRandomWord();
+    int currentguesscount = 0;
+    string input;
+    int currentTry = 0;
+    while (currentTry < Number_of_guessed)
+    {
+        do
+        {
+            cout << "Please enter your guess (word length must be " + to_string(Word_Length) + ") or type Q to quit: ";
+            getline(cin, input);
+            toUpperCase(input);
+        } while (input != "Q" && !isValid(input));
+        if (input == "Q")
+        {
+            cout << "Quit game" << endl;
+            break;
+        }
+        tries[currentTry] = input;
+        currentTry++;
+    }
 }
